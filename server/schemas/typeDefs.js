a89eb00cbf6ebf4a14d2b9d1ebf98847f0ed2afd
Anastasia Perez <Stasiperez85@gmail.com>
2022-05-01 17:34:03 -0600

    implement client-side graphql query and connect client and server folders

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,7 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
-type User {
+  type User {
     _id: ID
     username: String
     email: String
@@ -28,6 +26,11 @@ type User {
     username: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     me: User
     users: [User]
@@ -43,13 +46,6 @@ type User {
     addReaction(thoughtId: ID!, reactionBody: String!): Thought
     addFriend(friendId: ID!): User
   }
-
-  type Auth {
-      token: ID!
-      user: User
-  }
-  
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
