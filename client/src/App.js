4501ba0f16d69531ed5ae071653cdbbe74984104
Anastasia Perez <Stasiperez85@gmail.com>
2022-05-02 21:26:30 -0600

    correct profile broken code

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ function App() {
                 element={<Signup />}
               />
               <Route
-                path="/profile/:username?"
+                path="/profile/:username"
                 element={<Profile />}
               />
               <Route
